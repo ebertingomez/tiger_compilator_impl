ed99083043f88098a138fd0e43f3cec3e7e11d19
Enrique <enrique.gomez@telecom-paristech.fr>
2019-05-12 08:24:37 +0200

    WIP

--- lab2/dragon-tiger/src/ast/ast_evaluator.hh
+++ lab2/dragon-tiger/src/ast/ast_evaluator.hh
@@ -7,7 +7,7 @@
 
 namespace ast {
 
-class ASTEvaluator : public ConstASTVisitor {
+class ASTEvaluator : public ConstASTIntVisitor {
   std::ostream *ostream;
   bool verbose;
   unsigned indent_level = 0;
@@ -20,20 +20,20 @@ public:
     for (unsigned i = 0; i < indent_level; i++)
       *ostream << "  ";
   };
-  void visit(const IntegerLiteral &) override ;
-  void visit(const StringLiteral &) override ;
-  void visit(const BinaryOperator &) override ;
-  void visit(const Sequence &) override ;
-  void visit(const Let &) override ;
-  void visit(const Identifier &) override ;
-  void visit(const IfThenElse &) override ;
-  void visit(const VarDecl &) override ;
-  void visit(const FunDecl &) override ;
-  void visit(const FunCall &) override ;
-  void visit(const WhileLoop &) override ;
-  void visit(const ForLoop &) override ;
-  void visit(const Break &) override ;
-  void visit(const Assign &) override ;
+  int32_t visit(const IntegerLiteral &) override ;
+  int32_t visit(const StringLiteral &) override ;
+  int32_t visit(const BinaryOperator &) override ;
+  int32_t visit(const Sequence &) override ;
+  int32_t visit(const Let &) override ;
+  int32_t visit(const Identifier &) override ;
+  int32_t visit(const IfThenElse &) override ;
+  int32_t visit(const VarDecl &) override ;
+  int32_t visit(const FunDecl &) override ;
+  int32_t visit(const FunCall &) override ;
+  int32_t visit(const WhileLoop &) override ;
+  int32_t visit(const ForLoop &) override ;
+  int32_t visit(const Break &) override ;
+  int32_t visit(const Assign &) override ;
 };
 
 } // namespace ast
