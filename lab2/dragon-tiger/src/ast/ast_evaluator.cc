17201d9da853da67684ed1056a0cc9ad0639b721
Enrique <enrique.gomez@telecom-paristech.fr>
2019-05-12 18:36:17 +0200

    Merge of lab2 into master (implementation of the parser+lexer+evaluator)
    Squashed commit of the following:
    
    commit cb69bd3b662ead156a62af248879f613657dc310
    Author: Enrique <enrique.gomez@telecom-paristech.fr>
    Date:   Tue May 7 20:29:50 2019 +0200
    
        Import dragon-tiger for lab2

---
+++ lab2/dragon-tiger/src/ast/ast_evaluator.cc
@@ -0,0 +1,122 @@
+#include "../utils/errors.hh"
+#include "ast_evaluator.hh"
+
+namespace ast {
+
+int32_t ASTEvaluator::visit(const IntegerLiteral &literal) {
+    return literal.value;
+}
+
+int32_t ASTEvaluator::visit(const StringLiteral &literal) {
+    utils::error("The expression could not be evaluated");
+    return 0;
+}
+
+int32_t ASTEvaluator::visit(const BinaryOperator &binop) {
+    if (operator_name[binop.op]=="+")
+        return binop.get_left().accept(*this) + binop.get_right().accept(*this);
+    else if (operator_name[binop.op]=="-") 
+        return binop.get_left().accept(*this) - binop.get_right().accept(*this);
+    else if (operator_name[binop.op]=="*")
+        return binop.get_left().accept(*this) * binop.get_right().accept(*this);
+    else if (operator_name[binop.op]=="/") 
+        return binop.get_left().accept(*this) / binop.get_right().accept(*this);
+    else if (operator_name[binop.op]=="=") 
+        return ((binop.get_left().accept(*this) == binop.get_right().accept(*this))?1:0);
+    else if (operator_name[binop.op]=="<>") 
+        return ((binop.get_left().accept(*this) != binop.get_right().accept(*this))?1:0);
+    else if (operator_name[binop.op]=="<") 
+        return ((binop.get_left().accept(*this) < binop.get_right().accept(*this))?1:0);
+    else if (operator_name[binop.op]=="<=") 
+        return ((binop.get_left().accept(*this) <= binop.get_right().accept(*this))?1:0);
+    else if (operator_name[binop.op]==">") 
+        return ((binop.get_left().accept(*this) > binop.get_right().accept(*this))?1:0);
+    else if (operator_name[binop.op]==">=") 
+        return ((binop.get_left().accept(*this) >= binop.get_right().accept(*this))?1:0);
+    else if (operator_name[binop.op]=="|") 
+        return ((binop.get_left().accept(*this) || binop.get_right().accept(*this))?1:0);
+    else if (operator_name[binop.op]=="&") 
+        return ((binop.get_left().accept(*this) && binop.get_right().accept(*this))?1:0);
+    return 0;
+}
+
+int32_t ASTEvaluator::visit(const Sequence &seqExpr) {
+    if (seqExpr.get_exprs().size() > 0) {
+        const auto exprs = seqExpr.get_exprs();
+        for (auto expr = exprs.cbegin(); expr != exprs.cend(); expr++) {
+            if (*expr == NULL)
+                utils::error("Early error in the sequence");
+        }
+        Expr * e;
+        for (auto expr = exprs.cbegin(); expr != exprs.cend(); expr++) {
+            (*expr)->accept(*this);
+            e = *expr;      
+        }
+        return e->accept(*this);
+    } else {
+        utils::error("Empty sequence");
+    }
+    return 0;
+}
+
+int32_t ASTEvaluator::visit(const Let &let) {
+    utils::error("The expression could not be evaluated");
+    return 0;
+}
+
+int32_t ASTEvaluator::visit(const Identifier &id) {
+    utils::error("The expression could not be evaluated");
+    return 0;
+}
+
+int32_t ASTEvaluator::visit(const IfThenElse &ite) {
+    if (ite.get_condition().accept(*this))
+        return ite.get_then_part().accept(*this);
+    else {
+        const Expr * expr = &ite.get_else_part();
+        Sequence * seq = (Sequence *)expr;
+        if (seq->get_exprs().size() > 0)
+            return ite.get_else_part().accept(*this);
+        else
+            utils::error("Empty else");
+    }
+    return -1;
+
+}
+
+int32_t ASTEvaluator::visit(const VarDecl &decl) {
+    utils::error("The expression could not be evaluated");
+    return 0;
+}
+
+int32_t ASTEvaluator::visit(const FunDecl &decl) {
+    utils::error("The expression could not be evaluated");
+    return 0;
+}
+
+int32_t ASTEvaluator::visit(const FunCall &call) {
+    utils::error("The expression could not be evaluated");
+    return 0;
+}
+
+int32_t ASTEvaluator::visit(const WhileLoop &loop) {
+    utils::error("The expression could not be evaluated");
+    return 0;
+}
+
+int32_t ASTEvaluator::visit(const ForLoop &loop) {
+    utils::error("The expression could not be evaluated");
+    return 0;
+}
+
+int32_t ASTEvaluator::visit(const Break &brk) {
+    utils::error("The expression could not be evaluated");
+    return 0;
+}
+
+int32_t ASTEvaluator::visit(const Assign &assign) {
+    utils::error("The expression could not be evaluated");
+    return 0;
+}
+
+} // namespace ast
---
+++ lab2/dragon-tiger/src/ast/ast_evaluator.hh
@@ -0,0 +1,41 @@
+#ifndef AST_EVALUATOR_HH
+#define AST_EVALUATOR_HH
+
+#include <ostream>
+
+#include "nodes.hh"
+
+namespace ast {
+
+class ASTEvaluator : public ConstASTIntVisitor {
+  std::ostream *ostream;
+  bool verbose;
+  unsigned indent_level = 0;
+
+public:
+  ASTEvaluator(std::ostream *_ostream, bool _verbose)
+      : ostream(_ostream), verbose(_verbose) {}
+  void nl() {
+    *ostream << std::endl;
+    for (unsigned i = 0; i < indent_level; i++)
+      *ostream << "  ";
+  };
+  int32_t visit(const IntegerLiteral &) override ;
+  int32_t visit(const StringLiteral &) override ;
+  int32_t visit(const BinaryOperator &) override ;
+  int32_t visit(const Sequence &) override ;
+  int32_t visit(const Let &) override ;
+  int32_t visit(const Identifier &) override ;
+  int32_t visit(const IfThenElse &) override ;
+  int32_t visit(const VarDecl &) override ;
+  int32_t visit(const FunDecl &) override ;
+  int32_t visit(const FunCall &) override ;
+  int32_t visit(const WhileLoop &) override ;
+  int32_t visit(const ForLoop &) override ;
+  int32_t visit(const Break &) override ;
+  int32_t visit(const Assign &) override ;
+};
+
+} // namespace ast
+
+#endif // _AST_EVALUATOR_HH
