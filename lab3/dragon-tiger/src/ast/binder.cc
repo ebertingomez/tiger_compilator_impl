48c913c3ec815def1b0efec1bee502ec5282b360
Enrique <enrique.gomez@telecom-paristech.fr>
2019-05-16 00:40:32 +0200

    WIP

--- lab3/dragon-tiger/src/ast/binder.cc
+++ lab3/dragon-tiger/src/ast/binder.cc
@@ -168,8 +168,8 @@ void Binder::visit(Identifier &id) {
     error(id.loc, id.name.get() + " is not a function call");
   id.set_decl(decl);
   id.set_depth(static_cast<int>(scopes.size()));
-  if (id.get_depth() - id.get_decl()->get_depth() > 0)
-    id.get_decl()->set_escapes();
+  //if (id.get_depth() - decl->get_depth() > 0)
+  //  decl->set_escapes();
 }
 
 void Binder::visit(IfThenElse &ite) {
