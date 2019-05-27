633f8cb444a26af1e82c3bf7b32b4896d5b6cae8
Enrique <enrique.gomez@telecom-paristech.fr>
2019-05-27 10:49:17 +0200

    WIP

--- lab4/dragon-tiger/src/irgen/irgen-visitor.cc
+++ lab4/dragon-tiger/src/irgen/irgen-visitor.cc
@@ -95,8 +95,7 @@ llvm::Value *IRGenerator::visit(const VarDecl &decl) {
   allocations.insert(std::pair<const VarDecl *, llvm::Value *>(&decl,value));
   if (decl.get_escapes())
     frame_position.insert(std::pair<const VarDecl *, int>(&decl,decl.depth));
-
-  return alloca_in_entry(llvm_type(decl.get_type()),decl.name.get());;
+  llvm::Value pointer = alloca_in_entry(llvm_type(decl.get_type()),decl.name.get());
 }
 
 llvm::Value *IRGenerator::visit(const FunDecl &decl) {
