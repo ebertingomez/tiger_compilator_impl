454e94ce5d3602da279138b93ecabc305144aa70
Enrique Gomez <enrique.gomez@telecom-paristech.fr>
2019-05-27 11:17:01 +0200

    WIP

--- lab4/dragon-tiger/src/irgen/irgen-visitor.cc
+++ lab4/dragon-tiger/src/irgen/irgen-visitor.cc
@@ -93,11 +93,16 @@ llvm::Value *IRGenerator::visit(const VarDecl &decl) {
   llvm::Value * pointer = alloca_in_entry(llvm_type(decl.get_type()),decl.name.get());
 
   llvm::Value * value = decl.get_expr()->accept(*this);
-  allocations.insert(std::pair<const VarDecl *, llvm::Value *>(&decl,value));
-  if (decl.get_escapes())
-    frame_position.insert(std::pair<const VarDecl *, int>(&decl,decl.depth));
-    
-  Builder.CreateStore(value,pointer);
+
+  if (value != nullptr) {
+      allocations.insert(std::pair<const VarDecl *, llvm::Value *>(&decl,value));
+      if (decl.get_escapes())
+        frame_position.insert(std::pair<const VarDecl *, int>(&decl,decl.depth));
+  
+      Builder.CreateStore(value,pointer);
+  }
+
+  return nullptr;
 }
 
 llvm::Value *IRGenerator::visit(const FunDecl &decl) {
