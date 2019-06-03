6c4010ad20a949c65d4cc152a13263cc4cd6f132
Enrique <enrique.gomez@telecom-paristech.fr>
2019-06-04 00:12:03 +0200

    WIP

--- lab4/dragon-tiger/src/irgen/irgen.hh
+++ lab4/dragon-tiger/src/irgen/irgen.hh
@@ -67,7 +67,7 @@ class IRGenerator : public ConstASTValueVisitor {
 
   std::pair<llvm::StructType *, llvm::Value *> frame_up(int levels);
 
-  llvm::Value *IRGenerator::generate_vardecl(const VarDecl &decl);
+  llvm::Value *generate_vardecl(const VarDecl &decl);
 
   // Return the LLVM type corresponding to a Tiger type.
   llvm::Type *llvm_type(const ast::Type);
