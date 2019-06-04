d920f82fbac2de40a9ac50536db1cd6cc1e542e6
Enrique <enrique.gomez@telecom-paristech.fr>
2019-06-04 09:24:12 +0200

    WIP

--- lab4/dragon-tiger/src/irgen/irgen.cc
+++ lab4/dragon-tiger/src/irgen/irgen.cc
@@ -86,10 +86,10 @@ void IRGenerator::generate_function(const FunDecl &decl) {
       llvm::BasicBlock::Create(Context, "body", current_function);
 
   Builder.SetInsertPoint(bb2);
-
+  generate_frame();
   // Set the name for each argument and register it in the allocations map
   // after storing it in an alloca.
-  
+
   unsigned i = 0;
   for (auto &arg : current_function->args()) {
     arg.setName(params[i]->name.get());
