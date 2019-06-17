ab80d1393f6a9973b7d5573744adfd6772c79f87
Enrique Gomez <enrique.gomez@telecom-paristech.fr>
2019-06-17 10:48:42 +0200

    WIP

--- lab6/dragon-tiger/src/runtime/posix/runtime.c
+++ lab6/dragon-tiger/src/runtime/posix/runtime.c
@@ -52,9 +52,9 @@ int32_t __ord(const char *s) {
 
 const char *__chr(int32_t i) {
   char * s;
-  if ( (i <  0) || (i > 255)){
+  if ( (i <  0) || (i > 255))
     exit(EXIT_FAILURE);
-  }
+  
   else {
     if (i == 0){
       s = (char *) malloc(sizeof(char));
@@ -77,23 +77,50 @@ int32_t __size(const char *s) {
 }
 
 const char *__substring(const char *s, int32_t first, int32_t length) {
-  error("UNIMPLEMENTED __substring");
+  if ((first < 0) || (length < 0))
+    exit(EXIT_FAILURE);
+
+  int size = __size(s);
+  if (first + length > size)
+    exit(EXIT_FAILURE);
+  
+  char * new_s = (char *) malloc((length+1)*sizeof(char));
+
+  for (int i = 0; i< length ; i++)
+    new_s = s[first + i];
+  
+  new_s[first + length] = '\0';
+
+  return new_s;
 }
 
 const char *__concat(const char *s1, const char *s2) {
-  error("UNIMPLEMENTED __concat");
+  int size1 = __size(s1);
+  int size2 = __size(s2);
+
+  char * s = (char *) malloc((size1+size2+1)*sizeof(char));
+
+  for (int i = 0; i< size1 ; i++)
+    s = s1[i];
+  
+  for (int i = 0; i< size2 ; i++)
+    s = s[size1 + i];
+
+  s[size1 + size2] = '\0';
+
+  return s;
 }
 
 int32_t __strcmp(const char *s1, const char *s2) {
-  error("UNIMPLEMENTED __strcmp");
+  return strcmp(s1,s2);
 }
 
 int32_t __streq(const char *s1, const char *s2) {
-  error("UNIMPLEMENTED __streq");
+  return (strcmp(s1,s2) == 0) ? 1 : 0;
 }
 
 int32_t __not(int32_t i) {
-  error("UNIMPLEMENTED __not");
+  return (i == 0) ? 1 : 0;
 }
 
 void __exit(int32_t c) {
